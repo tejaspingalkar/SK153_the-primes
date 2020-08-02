54c2edd05f2863c9d8bd8d01287f67866a2891dc
Dipschaudhari <69034718+Dipschaudhari@users.noreply.github.com>
2020-08-02 16:17:18 +0530

    Update clusterreview.php

--- clusterreview.php
+++ clusterreview.php
@@ -40,7 +40,7 @@ var map = new google.maps.Map(document.getElementById('map'), {
 // Create an array of alphabetical characters used to label the markers.
 var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-// Add some markers to the map.
+// Add some markers to the map
 // Note: The code uses the JavaScript Array.prototype.map() method to
 // create an array of markers based on a given "locations" array.
 // The map() method here has nothing to do with the Google Maps API.
