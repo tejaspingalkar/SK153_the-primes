78c0d9ca03cef6198e19cf878c411142c3382cf6
tejaspingalkar <69034004+tejaspingalkar@users.noreply.github.com>
2020-08-03 09:51:09 +0530

    Add files via upload

---
+++ localmap.php
@@ -0,0 +1,14 @@
+<?php include "localheader.php";?>
+<?php include"conn.php";?>
+<div class="site-content">
+        <div class="row">
+            
+            <div class="col-sm-12">
+    <body>
+        <div id="aa">
+        <iframe src="https://internal.imd.gov.in/pages/city_weather_main_mausam.php" frameborder="0" width="100%"
+        height="900px"></iframe>
+    </div>
+        </div>
+        </div></div>   
+    <?php include "footer.php";?>
